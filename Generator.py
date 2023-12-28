dc8aaab0909ec2d2622cb35476195a84c89919e0
Ameya0612 <joshiameya1107@gmail.com>
2023-12-28 18:07:58 +0530

    adding the whole code resolved the error

--- Generator.py
+++ Generator.py
@@ -15,4 +15,35 @@ def generatePassword(pwlength):
             next_letter_index = random.randrange(len(alphabet))
             password = password + alphabet[next_letter_index]
         
+        password = replacewithnumber(password)
+        password = replacewithuppercaseletter(password)
+        
+        passwords.append(password) 
     
+    return passwords
+
+def main():
+    
+    numPasswords = int(input("How many passwords do you want to generate? "))
+    
+    print("Generating " +str(numPasswords)+" passwords")
+    
+    passwordLengths = []
+
+    print("Minimum length of password should be 3")
+
+    for i in range(numPasswords):
+        length = int(input("Enter the length of Password #" + str(i+1) + " "))
+        if length<3:
+            length = 3
+        passwordLengths.append(length)
+    
+    
+    Password = generatePassword(passwordLengths)
+
+    for i in range(numPasswords):
+        print ("Password #"+str(i+1)+" = " + Password[i])
+
+
+
+main()
